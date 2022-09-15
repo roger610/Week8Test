e5d332fec327338b6bda886e34cacdea8cbe3fe8
roger610 <110144081+roger610@users.noreply.github.com>
2022-09-15 12:41:10 +1000

    Add files via upload
    Updated climate.py

--- climate.py
+++ climate.py
@@ -1,9 +1,15 @@
 import matplotlib.pyplot as plt
+import sqlite3
+
         
 years = []
 co2 = []
 temp = []
 
+sqliteConnection = sqlite3.connect('climate.db')
+cursor = sqliteConnection.cursor()
+fetchA = cursor.fetchall()
+print(fetchA)
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
